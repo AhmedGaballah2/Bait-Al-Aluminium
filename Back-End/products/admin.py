9e72e376ca3c1f469bfcf66c6a4b6192ee1bccc2
Ahmed A. Gaballah <230941678+AhmedGaballah2@users.noreply.github.com>
2026-06-12 08:25:11 +0300

    Delete Back-End directory

--- Back-End/products/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Category, Product, Offer, NewArrival
-
-# Register your models here.
-
-admin.site.register(Product)
-admin.site.register(Offer)
-admin.site.register(NewArrival)
-admin.site.register(Category)
